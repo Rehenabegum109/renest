79c01eaa482426bab0945590b0d1144d08870eab
Rehena Begum <rehenebegum99900@gmail.com>
2026-07-10 22:02:53 +0600

    Fix vercel deployment setup

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,6 @@
-import app from "./app";
-import config from "./config";
-const PORT = config.port;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export {};
+// const PORT = config.port;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 
-import app from "./app";
-import config from "./config";
 
-const PORT = config.port;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+// const PORT = config.port;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
