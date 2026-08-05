32f0c31946e9e2dff69483924ba32400832a3098
Rehena Begum <rehenebegum99900@gmail.com>
2026-08-05 21:58:42 +0600

    fix production cookie cors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,54 +1,3 @@
-// import { Request, Response } from "express";
-// import { AuthService } from "./auth.service.js";
-
-
-// const register = async(req:Request,res:Response)=>{
-
-//     const result = await AuthService.registerUser(req.body);
-
-
-//     res.status(201).json({
-//         success:true,
-//         message:"User registered successfully",
-//         data:result
-//     });
-
-// }
-
-
-
-// const login = async(req:Request,res:Response)=>{
-
-//     const result = await AuthService.loginUser(req.body);
-
-
-//     res.json({
-//         success:true,
-//         message:"Login successful",
-//         data:result
-//     });
-
-// }
-
-// const getMe = async(req:any,res:Response)=>{
-
-//     const result = await AuthService.getMe(req.user.id);
-
-
-//     res.json({
-//         success:true,
-//         message:"Current user fetched successfully",
-//         data:result
-//     });
-
-// }
-
-// export const AuthController={
-//     register,
-//     login,
-//     getMe
-// }
-
 
 
 import { Request, Response, NextFunction } from "express";
@@ -62,12 +11,12 @@ const register = async (
   try {
     const result = await AuthService.registerUser(req.body);
 
-    res.cookie("accessToken", result.token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+   res.cookie("accessToken", result.token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(201).json({
       success: true,
@@ -89,11 +38,11 @@ const login = async (
 
   
     res.cookie("accessToken", result.token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
       success: true,
@@ -126,11 +75,11 @@ const logout = async (
   next: NextFunction
 ) => {
   try {
-    res.clearCookie("accessToken", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-    });
+   res.clearCookie("accessToken", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
     res.status(200).json({
       success: true,
