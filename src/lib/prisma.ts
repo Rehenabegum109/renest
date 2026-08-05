396991598d166a5d2fb7c653c73ba39f210ddceb
Rehena Begum <rehenebegum99900@gmail.com>
2026-08-05 17:58:15 +0600

    fix: use default prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,25 @@
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import "dotenv/config";
+// import { PrismaClient } from "../../generated/prisma/index.js";
+
+
+
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
-import { PrismaClient } from "../../generated/prisma/index.js";
 
-
-
-
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+export const prisma = new PrismaClient({
+  adapter,
+});
